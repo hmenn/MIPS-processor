50319ac705227921cdabe06788f2b322b2078965
hmenn <hmen.56@gmail.com>
2016-12-26 00:46:57 +0300

    instruction memory+pc update

--- mips_instr_mem.v
+++ mips_instr_mem.v
@@ -1,5 +1,5 @@
 module mips_instr_mem(instruction, program_counter);
-input [7:0] program_counter;
+input [31:0] program_counter;
 output [31:0] instruction;
 
 reg [31:0] instr_mem [255:0];
@@ -8,6 +8,8 @@ initial begin
 	$readmemb("instruction.mem", instr_mem);
 end
 
-assign instruction = instr_mem[program_counter];
+// siradaki instructionu getir
+// ilk 2 bit her zaman 0dır, word addressing
+assign instruction = instr_mem[program_counter[31:2]];
 
 endmodule
--- mips_instr_mem_test.v
+++ mips_instr_mem_test.v
@@ -1,22 +1,22 @@
 module mips_instr_mem_test();
 
 wire [31:0] inst;
-reg [7:0] pc;
+reg [31:0] pc;
 
 mips_instr_mem test(inst,pc);
 
 initial begin
-pc=8'b00000000;
+pc=32'b00000000000000000000000000000000;
 #20;
-pc=8'b00000001;
+pc=32'b00000000000000000000000000000100;
 #20;
-pc=8'b00000010;
+pc=32'b00000000000000000000000000001000;
 #20;
-pc=8'b00000011;
+pc=32'b00000000000000000000000000001100;
 end
 
 initial begin
-$monitor("time=%2d,PC=%8b,inst=%32b",$time,pc,inst);
+$monitor("time=%2d,PC=%32b,inst=%32b",$time,pc,inst);
 end
 
 
