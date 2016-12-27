8474ad6be7139348befb570ac608bc338cc9d0a5
hmenn <hmen.56@gmail.com>
2016-12-28 01:30:10 +0300

    mips registers module + test

--- mips_registers.v
+++ mips_registers.v
@@ -1,24 +1,32 @@
 module mips_registers
 (
-	output reg [31:0] read_data_1, read_data_2,
+	output [31:0] read_data_1, read_data_2,
 	input [31:0] write_data,
 	input [4:0] read_reg_1, read_reg_2, write_reg,
 	input signal_reg_write, clk
 );
-	reg [31:0] registers [31:0];
-	
+	reg [31:0] registers [0:31];
+
+	// simulasyon basinda registerleri bir defa oku dosyadan
 	initial begin
 		$readmemb("registers.mem", registers);
 	end
 
+
+	// pozitif sinyal atlamalarında calisir
+	// reg_write gelirse registere yazar
+	// TEST Bench yazilmistir
 	always @ (posedge clk)
 	begin
 		if (signal_reg_write) begin
 			registers[write_reg] <= write_data;
-		end
-		else begin
-			read_data_1 <= registers[read_reg_1];
-			read_data_2 <= registers[read_reg_2];
+			$display("time:%2d registere:%5b yazildi %32b",$time,write_reg,write_data);
 		end
 	end
+
+	//okuma yapar
+	assign read_data_1 = registers[read_reg_1];
+	assign read_data_2 = registers[read_reg_2];
+
+
 endmodule
---
+++ mips_registers_test.v
@@ -0,0 +1,37 @@
+module mips_registers_test();
+
+wire [31:0] read_data_1, read_data_2;
+reg [31:0] write_data;
+reg [4:0] read_reg_1, read_reg_2, write_reg;
+reg signal_reg_write, clk;
+
+mips_registers mr(read_data_1,read_data_2,write_data,read_reg_1,read_reg_2,write_reg,signal_reg_write,clk);
+
+
+initial begin
+  clk=0;
+  read_reg_1=0;
+  read_reg_2=0;
+  write_data=0;
+  write_reg=0;
+  signal_reg_write=0;
+
+  $monitor("Time:%g\t clk=%1b sig:%1b\n\treadreg1:%5b\treadreg2:%5b\twritereg:%5b\n\twritedata:%32b\n\treaddata1:%32b\n\treaddata2:%32b",
+            $time,clk,signal_reg_write,read_reg_1,read_reg_2,write_reg,
+            write_data,read_data_1,read_data_2);
+  #50 $finish;
+end
+
+
+always begin
+  #10
+  write_data+=1;
+  signal_reg_write=~signal_reg_write;
+  clk=~clk;
+  //read_reg_1+=1;
+  read_reg_2+=1;
+
+end
+
+
+endmodule
