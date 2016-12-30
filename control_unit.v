15f6818190d0c7a2914a545cdfdff00d7db48bc4
hmenn <hmen.56@gmail.com>
2016-12-30 19:35:41 +0300

    control unit alu signals added

--- control_unit.v
+++ control_unit.v
@@ -6,6 +6,9 @@
 // 4. MemWrite
 // 5. ALUSrc
 // 6. RegWrite
+
+//iverilog mips_core.v mips_testbench.v program_counter.v mips_instr_mem.v control_unit.v mips_registers.v extender_32bit.v mux_2_1.v alu_32bit.v alu_1bit.v msb_1bit.v mux_4_1.v
+
 module control_unit(ins,out_signals,ALUOp);
 
   input [5:0] ins;
@@ -15,40 +18,37 @@ module control_unit(ins,out_signals,ALUOp);
   output [2:0] ALUOp;
 
   localparam R_TYPE = 6'b000000;
+  //localparam J_TYPE = 6'b00001x;
+  localparam ADD = 6'b100000;
   localparam ADDI = 6'b001000;
   localparam ADDIU = 6'b001001;
   localparam ADDU = 6'b100001;
+  localparam AND = 6'b100100;
   localparam ANDI = 6'b001100;
   localparam BEQ = 6'b000100;
   localparam BNE = 6'b000101;
   localparam J = 6'b000010;
   localparam JAL = 6'b000011;
+  localparam JR = 6'b000000;
   localparam LBU = 6'b100100;
   localparam LHU = 6'b100101;
   localparam LUI = 6'b001111;
   localparam LW = 6'b100011;
+  localparam NOR = 6'b100111;
+  localparam OR = 6'b100101;
   localparam ORI = 6'b001101;
+  localparam SLT = 6'b101010;
   localparam SLTI = 6'b001010;
   localparam SLTIU = 6'b001011;
+  localparam SLTU = 6'b101011;
+  localparam SLL = 6'b000000;
+  localparam SRL = 6'b000010;
   localparam SB = 6'b101000;
   localparam SH = 6'b101001;
   localparam SW = 6'b101011;
+  localparam SUB = 6'b100010;
+  localparam SUBU = 6'b100011;
 
-  wire Rtype;
-  wire lw;
-  wire sw;
-  wire beq;
-
-  // Rtype op=000000
-  and andRtype(Rtype,~ins[5],~ins[4],~ins[3],~ins[2],~ins[1],~ins[0]);
-  // lw op=100011
-  and andlw(lw,ins[5],~ins[4],~ins[3],~ins[2],ins[1],ins[0]);
-  // sw op=101011
-  and andsw(sw,ins[5],~ins[4],ins[3],~ins[2],ins[1],ins[0]);
-  // beq op=000100
-  and andbeq(beq,~ins[5],~ins[4],~ins[3],ins[2],~ins[1],~ins[0]);
-
-  // initialize output signals
   //assign out_signals = {num_signals-1{1'b0}};
 
   // RegDst
@@ -63,8 +63,6 @@ module control_unit(ins,out_signals,ALUOp);
   // MemtoReg
   assign out_signals[3] = (ins== LW)? 1'b1: 1'b0;
 
-  // ALUOP
-  //TODO: ALU CONTROL HERE
   // MemWrite
   assign out_signals[4]= (ins== SW)? 1'b1: 1'b0;
 
@@ -72,13 +70,33 @@ module control_unit(ins,out_signals,ALUOp);
   assign out_signals[5]= ((ins== SW)||(ins==LW))? 1'b1: 1'b0;
 
   // RegWrite
-  assign out_signals[6] = ((ins== R_TYPE)||(ins==LW))? 1'b1: 1'b0;
+  // completed!
+  assign out_signals[6] = (
+                  (ins != J)|| // jump reg yazmaz
+                  (ins == R_TYPE)||
+                  (ins == ADDI)||
+                  (ins == ADDIU)||
+                  (ins == ANDI)||
+                  (ins == JAL)||
+                  (ins == LBU)||
+                  (ins == LHU)||
+                  (ins == LUI)||
+                  (ins == LW)||
+                  (ins == ORI)||
+                  (ins == SLTI)||
+                  (ins == SLTIU)
+                  )? 1'b1: 1'b0;
 
-
-
-
-
-
+  // ALUOP
+  assign ALUOp = (ins == R_TYPE) ? 3'b010 :
+                ((ins == BEQ)? 3'b001:
+                (((ins == LW)||(ins==SW)||(ins==ADDI)
+                  ||(ins==ADDIU)||(ins==LBU)||(ins==LHU)
+                  ||(ins==LUI)||(ins==SB)||(ins==SH))? 3'b000:
+                ((ins==NOR)? 3'b011:
+                ((ins==ANDI)? 3'b100:
+                ((ins==ORI)? 3'b101:
+                ((((ins==SLTI)||(ins==SLTIU))?3'b110: 3'b000)))))));
 
 
 
--- control_unit_test.v
+++ control_unit_test.v
@@ -1,3 +1,5 @@
+`include "control_unit.v"
+
 module control_unit_test();
 
 parameter num_signals=7;
@@ -5,23 +7,72 @@ wire [2:0] ALUOp;
 reg [5:0] ins;
 wire [num_signals-1:0] signals;
 
+localparam R_TYPE = 6'b000000;
+localparam J_TYPE = 6'b00001x;
+localparam ADD = 6'b100000;
+localparam ADDI = 6'b001000;
+localparam ADDIU = 6'b001001;
+localparam ADDU = 6'b100001;
+localparam AND = 6'b100100;
+localparam ANDI = 6'b001100;
+localparam BEQ = 6'b000100;
+localparam BNE = 6'b000101;
+localparam J = 6'b000010;
+localparam JAL = 6'b000011;
+localparam JR = 6'b000000;
+localparam LBU = 6'b100100;
+localparam LHU = 6'b100101;
+localparam LUI = 6'b001111;
+localparam LW = 6'b100011;
+localparam NOR = 6'b100111;
+localparam OR = 6'b100101;
+localparam ORI = 6'b001101;
+localparam SLT = 6'b101010;
+localparam SLTI = 6'b001010;
+localparam SLTIU = 6'b001011;
+localparam SLTU = 6'b101011;
+localparam SLL = 6'b000000;
+localparam SRL = 6'b000010;
+localparam SB = 6'b101000;
+localparam SH = 6'b101001;
+localparam SW = 6'b101011;
+localparam SUB = 6'b100010;
+localparam SUBU = 6'b100011;
 
 control_unit cnt(ins,signals,ALUOp);
 
 initial begin
-  ins =6'b000000; // Rtype
+  ins =R_TYPE; // Rtype
   #20;
-  ins =6'b100011; // lw
+  ins =LW; // lw
   #20;
-  ins =6'b101011; // sw
+  ins =SW; // sw
   #20;
-  ins =6'b000100; // beg
+  ins =BEQ; // beq
+  #20;
+  ins =NOR; // nor
+  #20;
+  ins =ANDI; // andi
+  #20;
+  ins =ORI; // ori
+  #20;
+  ins =SLTI; // slti
+  #20;
+  ins =SLTIU; // sltiu
+  #20;
+  ins =ADDI;
+  #20;
+  ins =LUI;
+  #20;
+  ins =J;
+  #20;
+  ins =JR;
   #20;
 end
 
 initial begin
-  $monitor("time=%2d, ins:%6b, RegDst:%1b,Branch:%1b,MemRead:%1b,MemtoReg:%1b,MemWrite:%1b,ALUSrc:%1b,RegWrite:%1b",
-        $time,ins,signals[0],signals[1],signals[2],signals[3],signals[4],signals[5],signals[6]);
+  $monitor("Time=%2d, OPCODE:%6b, ALUOp:%3b RegDst:%1b,Branch:%1b,MemRead:%1b,MemtoReg:%1b,MemWrite:%1b,ALUSrc:%1b,RegWrite:%1b",
+        $time,ins,ALUOp,signals[0],signals[1],signals[2],signals[3],signals[4],signals[5],signals[6]);
 end
 
 
